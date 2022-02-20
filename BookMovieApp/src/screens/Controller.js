e5522f80925e66f71058d3a4f3dd287a0a312e32
javed0024 <87536698+javed0024@users.noreply.github.com>
2022-02-20 20:51:01 +0530

    Details Functionality Added and entire code refactored

--- BookMovieApp/src/screens/Controller.js
+++ BookMovieApp/src/screens/Controller.js
@@ -1,7 +1,7 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
 import Home from "../screens/home/Home";
 import Details from "../screens/details/Details";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
